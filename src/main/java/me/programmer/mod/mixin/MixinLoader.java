c4e6e689d87ccec980d397e9132f875d0e8fac44
bluekitten <drjrsm.4201889@gmail.com>
2025-03-26 11:06:37 -0700

    Update MixinLoader.java

--- src/main/java/me/programmer/mod/mixin/MixinLoader.java
+++ src/main/java/me/programmer/mod/mixin/MixinLoader.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 @IFMLLoadingPlugin.Name("Mod")
 @IFMLLoadingPlugin.SortingIndex(1002)
-@IFMLLoadingPlugin.TransformerExclusions({ "me.programmer.mod.mixin.client", "me.programmer.mod.mixin.block", "me.programmer.mod.accessor" })
+@IFMLLoadingPlugin.TransformerExclusions({ "me.programmer.mod.mixin.client", "me.programmer.mod.mixin.block", "me.programmer.mod.mixin.accessor" })
 public class MixinLoader implements IFMLLoadingPlugin
 {
     public static final Logger log;
